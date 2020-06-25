3e3a9b68f96d9373c494f530b0755484d09dbdff
barak <barak1212127@gmail.com>
2020-06-25 13:35:32 +0300

    Updates

---
+++ precache-manifest.a8aa8101d18d0ee3c3f6121cc2535d8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c6b2db32ec02355835dbb7aa853efe",
+    "url": "/Barak-Cohen-22-06-2020/index.html"
+  },
+  {
+    "revision": "e4c684aeac2b6f7eb68c",
+    "url": "/Barak-Cohen-22-06-2020/static/css/main.df9d7ffa.chunk.css"
+  },
+  {
+    "revision": "250bcbc840bd9ee0bfb2",
+    "url": "/Barak-Cohen-22-06-2020/static/js/2.c61e61b5.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Barak-Cohen-22-06-2020/static/js/2.c61e61b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c684aeac2b6f7eb68c",
+    "url": "/Barak-Cohen-22-06-2020/static/js/main.1f482610.chunk.js"
+  },
+  {
+    "revision": "7573272f687fd17326a7",
+    "url": "/Barak-Cohen-22-06-2020/static/js/runtime-main.52b3b6ce.js"
+  },
+  {
+    "revision": "71e6cc865c3a861a15dfd03302ecfa63",
+    "url": "/Barak-Cohen-22-06-2020/static/media/heroloLogo.71e6cc86.jpg"
+  },
+  {
+    "revision": "7322d5b92466db69b1d7f78558a84766",
+    "url": "/Barak-Cohen-22-06-2020/static/media/weatherImage.7322d5b9.jpg"
+  }
+]);
